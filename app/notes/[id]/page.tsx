5cfae2ff0591c3346c81ad781803e112716f1d74
dima1310 <dimaponomarenko13101981@gmail.com>
2025-08-04 17:15:49 +0300

    up type

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,15 +1,12 @@
 type NoteDetailsPageProps = {
-  params: {
-    id: string;
-  };
+  params: { id: string };
 };
 
 export default function NoteDetailsPage({ params }: NoteDetailsPageProps) {
-  const { id } = params;
-
   return (
-    <main>
-      <h1>Note ID: {id}</h1>
-    </main>
+    <div>
+      <h1>Note Details Page</h1>
+      <p>Note ID: {params.id}</p>
+    </div>
   );
 }
