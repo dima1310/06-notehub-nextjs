d0722c047e9970c7cace3deead0711ea63ccaf03
dima1310 <dimaponomarenko13101981@gmail.com>
2025-08-04 17:10:56 +0300

    up page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,33 +1,15 @@
-import { fetchNoteById } from "@/lib/api";
-import type { Note } from "@/types/note";
+type NoteDetailsPageProps = {
+  params: {
+    id: string;
+  };
+};
 
-interface NoteDetailsPageProps {
-  params: { id: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}
-
-export default async function NoteDetailsPage({
-  params,
-}: NoteDetailsPageProps) {
+export default function NoteDetailsPage({ params }: NoteDetailsPageProps) {
   const { id } = params;
 
-  let note: Note | null = null;
-
-  try {
-    note = await fetchNoteById(Number(id));
-  } catch (error) {
-    console.error("Failed to fetch note:", error);
-    return <div>Ошибка при загрузке заметки</div>;
-  }
-
-  if (!note) {
-    return <div>Заметка не найдена</div>;
-  }
-
   return (
-    <div>
-      <h1>{note.title}</h1>
-      <p>{note.content}</p>
-    </div>
+    <main>
+      <h1>Note ID: {id}</h1>
+    </main>
   );
 }
